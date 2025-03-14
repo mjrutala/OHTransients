7e6899b767f6c3cde2b32d9b74c19c573e687da0
mjrutala <mrutala@bu.edu>
2025-03-14 14:43:07 -0400

    Code to parse DONKI databse for CMEs and ICMEs, which are found bu filtering IPS for attached CME events; this is preferable to the current HUXt DONKI reader, which does not appear to filter

---
+++ code/queryDONKI.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  4 15:14:05 2025
+
+@author: mrutala
+"""
+
+def CME(start, end):
+    """
+    Read the DONKI CME list
+    """
+    import pandas as pd
+    import requests
+    import datetime
+    import numpy as np
+    
+    strptime = datetime.datetime.strptime
+
+    # Construct URL for the request
+    baseurl = "https://kauai.ccmc.gsfc.nasa.gov/DONKI/WS/get/CME"
+    start_str = "?startDate=" + start.strftime("%Y-%m-%d")
+    end_str = "&endDate=" + end.strftime("%Y-%m-%d")
+    url = baseurl + start_str + end_str
+    
+    # Check for a response and get the data
+    response = requests.get(url)
+    if response.status_code != 200:
+        print('cannot successfully get an http response')
+        
+    # read the data
+    print("Getting data from", url)
+    df = pd.read_json(url)
+    
+    # Get useful datetimes
+    time_fmt = "%Y-%m-%dT%H:%MZ"
+    df['startTime'] = [strptime(t, time_fmt) for t in df['startTime'].to_list()]
+    
+    # Must have CME analysis:
+    notAnalyzed_index = []
+    for index, row in df.iterrows():
+        if row['cmeAnalyses'] is None:
+            notAnalyzed_index.append(index)
+    df = df.drop(notAnalyzed_index, axis='index')
+    
+    # Only keep the most recent CME analysis, and simplify instruments & linkedEvents
+    for index, row in df.iterrows():
+        cmeAnalysis_datetimes = [strptime(d['submissionTime'], time_fmt) for d in row['cmeAnalyses']]
+        most_recent = np.argmax(cmeAnalysis_datetimes)   
+        
+        
+        # Keep the most recent analysis
+        row['cmeAnalyses'] = row['cmeAnalyses'][most_recent]
+        # Drop the ENLIL model results?
+        _ = row['cmeAnalyses'].pop('enlilList')
+        # Flatten the instrument names
+        row['instruments'] = [d['displayName'] for d in row['instruments']]
+        # Flatten the linked events
+        if row['linkedEvents'] != None:
+            row['linkedEvents'] = [d['activityID'] for d in row['linkedEvents']]
+        else:
+            row['linkedEvents'] = []
+        df.loc[index] = row
+            
+    df = df.drop(index)
+    return df
+
+def ICME(start, end, location='Earth'):
+    """
+    Read the DONKI InterPlanetary Shock (IPS) list, and optionally cross-
+    reference with the DONKI CME list to identify ICMEs
+    """
+    import pandas as pd
+    import requests
+    import datetime
+    import numpy as np
+    
+    strptime = datetime.datetime.strptime
+
+    # Construct URL for the request
+    baseurl = "https://kauai.ccmc.gsfc.nasa.gov/DONKI/WS/get/IPS"
+    start_str = "?startDate=" + start.strftime("%Y-%m-%d")
+    end_str = "&endDate=" +  end.strftime("%Y-%m-%d")
+    location_str = "&location=" + location
+    url = baseurl + start_str + end_str + location_str
+    
+    # Check for a response and get the data
+    response = requests.get(url)
+    if response.status_code != 200:
+        print('cannot successfully get an http response')
+        
+    # read the data
+    print("Getting data from", url)
+    df = pd.read_json(url)
+    
+    # Get useful datetimes
+    time_fmt = "%Y-%m-%dT%H:%MZ"
+    df['eventTime'] = [strptime(t, time_fmt) for t in df['eventTime'].to_list()]
+    
+    for index, row in df.iterrows():
+        # Flatten the instrument names
+        row['instruments'] = [d['displayName'] for d in row['instruments']]
+        df.loc[index] = row
+        
+    # ICMEs must be linked to CMEs; drop IPSs that aren't
+    delta = datetime.timedelta(days = 7)
+    cme_df = CME(start - delta, end)
+    df['linkedEvents'] = ''
+    for index, row in df.iterrows():
+        matches = [row['activityID'] in l for l in cme_df['linkedEvents']]
+        
+        if np.array(matches).any() == False:
+            df = df.drop(index, axis='index')
+        else:
+            row['linkedEvents'] = cme_df.loc[matches, 'activityID'].values[0]
+            df.loc[index] = row
+    
+
+    return df.reset_index()
